577862d61701e1373ea57cdb06c8bf1f9deecf2d
oosakiharuki <k023g0035@g.neec.ac.jp>
2025-10-13 22:46:40 +0900

    DirectXTexのRelease版がビルドできてなかったらしい+タイトルシ－ンのループモード

--- project/GameProgram/Scene/SelectScene.cpp
+++ project/GameProgram/Scene/SelectScene.cpp
@@ -16,7 +16,10 @@ void SelectScene::Initialize() {
 }
 
 void SelectScene::Update() {
-
+	
+	//すぐにタイトルに
+	sceneNo = Title;
+	
 	if (isfadeStart) {
 		FadeScreen::GetInstance()->FedeIn();
 		if (!FadeScreen::GetInstance()->GetIsFadeing()) {
--- project/GameProgram/Scene/TitleScene.cpp
+++ project/GameProgram/Scene/TitleScene.cpp
@@ -112,7 +112,7 @@ void TitleScene::Update() {
 
 	}
 	else {
-		wts[0].translation_.y -= gravity;
+		wts[0].translation_.y -= gravity - 1.0f;
 	}
 
 	sprite_Moji_Title->SetPosition(Title_pos);
